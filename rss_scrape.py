8a90eea41eb828e680866d8aef41cde83b11ab2f
John B <john.b30@gmail.com>
2013-01-27 22:34:52 -0500

    added steps to log progress

--- rss_scrape.py
+++ rss_scrape.py
@@ -2,6 +2,7 @@ from apscheduler.scheduler import Scheduler
 #from joblib import Parallel, delayed
 from pymongo import MongoClient
 import pattern.web 
+import datetime
 import time
 import pages_scrape
 import mongo_connection
@@ -32,13 +33,16 @@ def scrape_func(address, website):
     db = connection.atrocities_data
     collection = db[website] 
 
-    results = pattern.web.Newsfeed().search(address, count=100)
+    log = open('log_file.txt', 'a')
+
+    results = pattern.web.Newsfeed().search(address, count=100, cached=False)
     keep = ('kill', 'bomb', 'die', 'attack', 'shoot', 'fight')
     ignore = ('crash', 'accident', 'funeral', 'flood', 'house fire', 
             'apartment fire', 'lightning', 'mine blast', 'lion', 'disease',
             'ebola', 'cholera', 'murder-suicide', 'hit-and-run', 
             'half-staff', 'video:', 'blog')
-    print 'There are %d results from %s \n' % (len(results), website)
+    log1 = 'There are %d results from %s \n' % (len(results), website)
+    log.write(log1)
     for result in results:
         toWrite = (result.title + ' ' 
                 + pattern.web.plaintext(result.description))
@@ -50,61 +54,77 @@ def scrape_func(address, website):
                 entry_id = mongo_connection.add_entry(collection, temp, 
                         result.url, result.date, website)
                 if entry_id:
-                    print 'Added entry from %s with id %s \n' % (result.url,
+                    log2 = 'Added entry from %s with id %s \n' % (result.url,
                             str(entry_id))
+                    log.write(log2)
                 else:
-                    print 'Result from %s already in database \n' % (result.url)
+                    log2 = 'Result from %s already in database \n' % (result.url)
+                    log.write(log2)
             if website == 'bbc':
                 temp = pages_scrape.bbc_scrape(result.url, result.title,
                             result.date)
                 entry_id = mongo_connection.add_entry(collection, temp, 
                         result.url, result.date, website)
                 if entry_id:
-                    print 'Added entry from %s with id %s \n' % (result.url,
+                    log2 = 'Added entry from %s with id %s \n' % (result.url,
                             str(entry_id))
+                    log.write(log2)
                 else:
-                    print 'Result from %s already in database \n' % (result.url)
+                    log2 = 'Result from %s already in database \n' % (result.url)
+                    log.write(log2)
             if website == 'reuters':
                 temp = pages_scrape.reuters_scrape(result.url, result.title,
                         result.date)
                 entry_id = mongo_connection.add_entry(collection, temp, 
                         result.url, result.date, website)
                 if entry_id:
-                    print 'Added entry from %s with id %s \n' % (result.url,
+                    log2 = 'Added entry from %s with id %s \n' % (result.url,
                             str(entry_id))
+                    log.write(log2)
                 else:
-                    print 'Result from %s already in database \n' % (result.url)
+                    log2 = 'Result from %s already in database \n' % (result.url)
+                    log.write(log2)
             if website == 'ap':
                 temp = pages_scrape.ap_scrape(result.url, result.title,
                         result.date)
                 entry_id = mongo_connection.add_entry(collection, temp, 
                         result.url, result.date, website)
                 if entry_id:
-                    print 'Added entry from %s with id %s \n' % (result.url,
+                    log2 = 'Added entry from %s with id %s \n' % (result.url,
                             str(entry_id))
+                    log.write(log2)
                 else:
-                    print 'Result from %s already in database \n' % (result.url)
+                    log2 = 'Result from %s already in database \n' % (result.url)
+                    log.write(log2)
             if website == 'upi':
                 temp = pages_scrape.upi_scrape(result.url, result.title,
                         result.date)
                 entry_id = mongo_connection.add_entry(collection, temp, 
                         result.url, result.date, website)
                 if entry_id:
-                    print 'Added entry from %s with id %s \n' % (result.url,
+                    log2 = 'Added entry from %s with id %s \n' % (result.url,
                             str(entry_id))
+                    log.write(log2)
                 else:
-                    print 'Result from %s already in database \n' % (result.url)
+                    log2 = 'Result from %s already in database \n' % (result.url)
+                    log.write(log2)
             if website == 'google':
                 temp = (result.title + '\n' + result.date + '\n\n' +
                     pattern.web.plaintext(result.description))
                 entry_id = mongo_connection.add_entry(collection, temp, 
                         result.url, result.date, website)
                 if entry_id:
-                    print 'Added entry from %s with id %s \n' % (result.url,
+                    log2 = 'Added entry from %s with id %s \n' % (result.url,
                             str(entry_id))
+                    log.write(log2)
                 else:
-                    print 'Result from %s already in database \n' % (result.url)
-    print '+' * 70 + '\nScraped %s once!\n' % (website)
+                    log2 = 'Result from %s already in database \n' % (result.url)
+                    log.write(log2)
+    interupt = '+' * 70
+    log3 = '%s\nScrape %s once at %s!\n%s\n' % (interupt, website, 
+            datetime.datetime.now(), interupt)
+    log.write(log3)
+    log.close()
 
 #def parallel_func(siteList, database, n_cores = -1):
 #    """
@@ -124,6 +144,7 @@ def scrape_func(address, website):
 def call_scrape_func(siteList):
     for website in siteList:
         scrape_func(siteList[website], website)
+    print 'Scraped at %s' % datetime.datetime.now()
 
 if __name__ == '__main__':
     print 'Running...'
