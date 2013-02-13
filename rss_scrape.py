cde716d04e75076c2749442d8cd5eee38381f4fd
John B <john.b30@gmail.com>
2013-02-12 22:18:51 -0500

    pep8
    pep8 compliance

--- rss_scrape.py
+++ rss_scrape.py
@@ -1,7 +1,7 @@
 from apscheduler.scheduler import Scheduler
 #from joblib import Parallel, delayed
 from pymongo import MongoClient
-import pattern.web 
+import pattern.web
 import datetime
 import time
 import pages_scrape
@@ -14,11 +14,12 @@ import logging
 # If 'keep' is more than one word, change the 'all()' to 'any() and all()' since
 # the desired behavior is to have any of the keep and none of the ignore.
 
+
 def scrape_func(address, website):
     """
     Function to scrape various RSS feeds. Uses the 'keep' and 'ignore' iterables
     to define which words should be used in the text search.
-    
+
     Inputs
     ------
     address : address for the RSS feed to scrape. String.
@@ -31,28 +32,28 @@ def scrape_func(address, website):
     """
     connection = MongoClient()
     db = connection.atrocities_data
-    collection = db[website] 
+    collection = db[website]
 
     log = open('log_file.txt', 'a')
     results = pattern.web.Newsfeed().search(address, count=100, cached=False)
     keep = ('kill', 'bomb', 'die', 'attack', 'shoot', 'fight', 'slain',
-    'perished')
-    ignore = ('crash', 'accident', 'funeral', 'flood', 'house fire', 
-            'apartment fire', 'lightning', 'mine blast', 'lion', 'disease',
-            'ebola', 'cholera', 'murder-suicide', 'hit-and-run', 
-            'half-staff', 'video:', 'blog')
+            'perished')
+    ignore = ('crash', 'accident', 'funeral', 'flood', 'house fire',
+                'apartment fire', 'lightning', 'mine blast', 'lion', 'disease',
+                'ebola', 'cholera', 'murder-suicide', 'hit-and-run',
+                'half-staff', 'video:', 'blog')
     log1 = 'There are %d results from %s \n' % (len(results), website)
     log.write(log1)
     for result in results:
-        toWrite = (pattern.web.plaintext(result.title) + ' ' 
-                + pattern.web.plaintext(result.description))
+        toWrite = (pattern.web.plaintext(result.title) + ' '
+                    + pattern.web.plaintext(result.description))
 
-        if (any([term in toWrite.lower() for term in keep]) and 
+        if (any([term in toWrite.lower() for term in keep]) and
                 all([word not in toWrite.lower() for word in ignore])):
             if website == 'nyt':
                 temp = pages_scrape.nyt_scrape(result.url, result.title)
-                entry_id = mongo_connection.add_entry(collection, temp, 
-                        result.url, result.date, website)
+                entry_id = mongo_connection.add_entry(collection, temp,
+                                    result.url, result.date, website)
                 if entry_id:
                     log2 = 'Added entry from %s with id %s \n' % (result.url,
                             str(entry_id))
@@ -63,7 +64,7 @@ def scrape_func(address, website):
             if website == 'bbc':
                 temp = pages_scrape.bbc_scrape(result.url, result.title,
                             result.date)
-                entry_id = mongo_connection.add_entry(collection, temp, 
+                entry_id = mongo_connection.add_entry(collection, temp,
                         result.url, result.date, website)
                 if entry_id:
                     log2 = 'Added entry from %s with id %s \n' % (result.url,
@@ -75,7 +76,7 @@ def scrape_func(address, website):
             if website == 'reuters':
                 temp = pages_scrape.reuters_scrape(result.url, result.title,
                         result.date)
-                entry_id = mongo_connection.add_entry(collection, temp, 
+                entry_id = mongo_connection.add_entry(collection, temp,
                         result.url, result.date, website)
                 if entry_id:
                     log2 = 'Added entry from %s with id %s \n' % (result.url,
@@ -87,7 +88,7 @@ def scrape_func(address, website):
             if website == 'ap':
                 temp = pages_scrape.ap_scrape(result.url, result.title,
                         result.date)
-                entry_id = mongo_connection.add_entry(collection, temp, 
+                entry_id = mongo_connection.add_entry(collection, temp,
                         result.url, result.date, website)
                 if entry_id:
                     log2 = 'Added entry from %s with id %s \n' % (result.url,
@@ -99,7 +100,7 @@ def scrape_func(address, website):
             if website == 'upi':
                 temp = pages_scrape.upi_scrape(result.url, result.title,
                         result.date)
-                entry_id = mongo_connection.add_entry(collection, temp, 
+                entry_id = mongo_connection.add_entry(collection, temp,
                         result.url, result.date, website)
                 if entry_id:
                     log2 = 'Added entry from %s with id %s \n' % (result.url,
@@ -111,9 +112,9 @@ def scrape_func(address, website):
             if website == 'xinhua':
                 page_url = result.url.encode('ascii')
                 page_url = page_url.replace('"', '')
-                temp = pages_scrape.xinhua_scrape(page_url,result.title, 
+                temp = pages_scrape.xinhua_scrape(page_url, result.title,
                         result.date)
-                entry_id = mongo_connection.add_entry(collection, temp, 
+                entry_id = mongo_connection.add_entry(collection, temp,
                         result.url, result.date, website)
                 if entry_id:
                     log2 = 'Added entry from %s with id %s \n' % (result.url,
@@ -125,7 +126,7 @@ def scrape_func(address, website):
             if website == 'google':
                 temp = (result.title + '\n' + result.date + '\n\n' +
                     pattern.web.plaintext(result.description))
-                entry_id = mongo_connection.add_entry(collection, temp, 
+                entry_id = mongo_connection.add_entry(collection, temp,
                         result.url, result.date, website)
                 if entry_id:
                     log2 = 'Added entry from %s with id %s \n' % (result.url,
@@ -135,7 +136,7 @@ def scrape_func(address, website):
                     log2 = 'Result from %s already in database \n' % (result.url)
                     log.write(log2)
     interupt = '+' * 70
-    log3 = '%s\nScrape %s once at %s!\n%s\n' % (interupt, website, 
+    log3 = '%s\nScrape %s once at %s!\n%s\n' % (interupt, website,
             datetime.datetime.now(), interupt)
     log.write(log3)
     log.close()
@@ -148,12 +149,13 @@ def scrape_func(address, website):
 #    ------
 #
 #    siteList : Dictionary of sites to be scraped with the website name as the
-#    key and address as the value. Dict. 
+#    key and address as the value. Dict.
 #
-#    n_cores : Number of cores to use. -1 indicates all available. Integer. 
+#    n_cores : Number of cores to use. -1 indicates all available. Integer.
 #    """
 #    Parallel(n_jobs=n_cores)(delayed(scrape_func)(siteList[website], website,
 #        database) for website in siteList)
+
 
 def call_scrape_func(siteList):
     for website in siteList:
@@ -163,21 +165,21 @@ def call_scrape_func(siteList):
 if __name__ == '__main__':
     print 'Running...'
     to_scrape = {
-            'google' : 'https://news.google.com/?output=rss', 
-            'nyt' : 'http://rss.nytimes.com/services/xml/rss/nyt/World.xml',
-            'reuters' : 'http://feeds.reuters.com/Reuters/worldNews',
-            'bbc' : 'http://feeds.bbci.co.uk/news/world/rss.xml',
-            'ap' : 'http://hosted2.ap.org/atom/APDEFAULT/cae69a7523db45408eeb2b3a98c0c9c5',
-            'upi' : 'http://rss.upi.com/news/emerging_threats.rss',
-            'xinhua' : 'http://www.xinhuanet.com/english/rss/worldrss.xml'
-            }
+            'google': 'https://news.google.com/?output=rss',
+            'nyt': 'http://rss.nytimes.com/services/xml/rss/nyt/World.xml',
+            'reuters': 'http://feeds.reuters.com/Reuters/worldNews',
+            'bbc': 'http://feeds.bbci.co.uk/news/world/rss.xml',
+            'ap': 'http://hosted2.ap.org/atom/APDEFAULT/cae69a7523db45408eeb2b3a98c0c9c5',
+            'upi': 'http://rss.upi.com/news/emerging_threats.rss',
+            'xinhua': 'http://www.xinhuanet.com/english/rss/worldrss.xml'
+                }
     #Line to aid in debugging
     call_scrape_func(to_scrape)
 
     logging.basicConfig()
-    
+
     sched = Scheduler()
-    sched.add_interval_job(call_scrape_func, args = [to_scrape], hours=1)
+    sched.add_interval_job(call_scrape_func, args=[to_scrape], hours=1)
     sched.start()
     while True:
         time.sleep(10)
